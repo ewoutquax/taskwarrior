0b6e2d4223d1e8c9614a86011dbee7aaa145642e
Ewout Quax <ewout.quax@quicknet.nl>
2016-09-18 08:22:34 +0200

    Add basic-authentication
    Because the whole world can now see and enter tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::TasksController < ApplicationController
+class Api::V1::TasksController < Api::BaseController
   def show
     values = Fetcher::Task.by_uuid(params[:id])
     render status: :ok, json: values.to_hash
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  USERS = { 'task' => 'warrior' }.freeze
+
+  before_action :authenticate
+
+  def authenticate
+    return unless Rails.env.production?
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
+      end
+  end
 end
